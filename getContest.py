3cf2f1b0bed8b95a1a629d3cb7babac44c2c3073
tacoder <abhinav.singh21093@gmail.com>
2015-12-27 01:36:35 +0530

    Added more modularity

--- getContest.py
+++ getContest.py
@@ -6,15 +6,30 @@ CODECHEF_PROBLEM_URL = 'https://www.codechef.com/problems/'
 
 #contestCode = "DEC15"
 
-def getContestJsonData(contestCode):
+def getContestJsonDataFromUrl(contestCode):
     return json.load(urlopen(CODECHEF_API_URL+contestCode))#open('DEC15' + '.json'))
 
+def getContestJsonDataFromFile(fileName):
+    return json.loads(open(fileName))
 
-def getContest(contestCode):
-    data = getContestJsonData(contestCode)
+def getInputOutputFromHtml(body):
+    body = body[body.find("<h3>Example</h3>"):]
+    body = body[body.find("<pre>"):body.find("</pre>")]
+    body = re.sub("<.*?>", "", body)
+    inputString = re.sub("\n\n","\n",body[body.find("Input:")+7:body.find("Output")])
+    outputString = re.sub("\n\n","\n",body[body.find("Output:")+8:])
+    print(contestCode)
+    print("Input string is :")
+    print(inputString)
+    print("Output string is :")
+    print(outputString)
+    print('******************************************')
+    return (inputString,outputString)
+
+def getContest(contestCode,data):
     problemData = {}
     if 'problems_data' not in data:
-        #p               rint("Problems not embedded. Fetching individual problems:")
+        #print("Problems not embedded. Fetching individual problems:")
         for problem in data['problems']:
             print(problem)
             problemData[str(problem)] = {}
@@ -24,21 +39,11 @@ def getContest(contestCode):
     if not os.path.exists(contestCode):
             os.makedirs(contestCode)
     for problem in problemData:
-        body = problemData[problem]["body"]
-        body = body[body.find("<h3>Example</h3>"):]
-        body = body[body.find("<pre>"):body.find("</pre>")]
-        body = re.sub("<.*?>", "", body)
-        inputString = re.sub("\n\n","\n",body[body.find("Input:")+7:body.find("Output")])
-        outputString = re.sub("\n\n","\n",body[body.find("Output:")+8:])
-        print("Input string is :")
-        print(inputString)
-        print("Output string is :")
-        print(outputString)
-        print('******************************************')
+        inputString,outputString = getInputOutputFromHtml(problemData[problem]["body"])
         inputFile = open(contestCode + "/" + problem + ".in",'w')
         inputFile.write(inputString)
         outputFile = open(contestCode + "/" + problem + ".out",'w')
         outputFile.write(outputString)
 
 
-getContest("DEC15");
+getContest("DEC15",getContestJsonDataFromUrl(contestCode));
